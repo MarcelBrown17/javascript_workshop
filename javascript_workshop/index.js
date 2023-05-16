9439b48e9408592531fa793060fc206f200de2d3
MarcelBrown17 <130047013+MarcelBrown17@users.noreply.github.com>
2023-05-16 16:31:12 +0200

    Add files via upload

--- javascript_workshop/index.js
+++ javascript_workshop/index.js
@@ -1,1 +1,5 @@
-le
+let subjects = [ "python", "C++", "VB.net", "Javascript", "Assembly"];
+console.log(subjects)
+console.log("first subject",subjects[0])
+console.log("Last subject",subjects[4])
+console.log("middle subject",subjects[2])
