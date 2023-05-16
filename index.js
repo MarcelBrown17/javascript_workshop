2f752e0abb067a39520dc99d99b8ba9585c5d0da
MarcelBrown17 <130047013+MarcelBrown17@users.noreply.github.com>
2023-05-16 16:30:31 +0200

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-let subjects = [ "python", "C++", "VB.net", "Javascript", "Assembly"];
-console.log(subjects)
-console.log("first subject",subjects[0])
-console.log("Last subject",subjects[4])
-console.log("middle subject",subjects[2])
